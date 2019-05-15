eafab4bc153f1490e16c410d76c853e8496d45cf
wangyang <16301162@bjtu.edu.cn>
2019-05-15 18:50:20 +0800

    add Guava RateLimiter

--- src/main/java/edu/bjtu/demo/service/ReserveServiceImpl.java
+++ src/main/java/edu/bjtu/demo/service/ReserveServiceImpl.java
@@ -4,10 +4,12 @@ import edu.bjtu.demo.domain.UserCoach;
 import edu.bjtu.demo.repository.UserCoachRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.google.common.util.concurrent.RateLimiter;
 
 @Service
 public class ReserveServiceImpl implements ReserveService{
     private UserCoachRepository UserCoachRepository;
+    private RateLimiter rateLimiter = RateLimiter.create(1); // rate is "10 permits per second"
 
     @Autowired
     public void setUserCoachRepository(UserCoachRepository UserCoachRepository) {
@@ -16,16 +18,19 @@ public class ReserveServiceImpl implements ReserveService{
 
     @Override
     public UserCoach reserve(UserCoach usercoach) {
+        rateLimiter.acquire();
         return this.UserCoachRepository.save(usercoach);
     }
 
     @Override
     public void cancel(UserCoach usercoach) {
+        rateLimiter.acquire();
         this.UserCoachRepository.delete(usercoach);
     }
 
     @Override
     public Iterable<UserCoach> listOrders() {
+        rateLimiter.acquire();
         return this.UserCoachRepository.findAll();
     }
 
